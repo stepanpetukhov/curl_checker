ada29069af27aa7d219c0f5b26ca70b1a197b7b9
Степан С. Петухов <stepanpetuhov@gmail.com>
2020-09-21 22:04:59 +0300

    Some fix in curl.sh and add sample input and output files

--- curl.sh
+++ curl.sh
@@ -2,5 +2,5 @@
 date > result.csv
 while read LINE; do
   echo "$LINE" done!
-  curl -o /dev/null --silent --head --write-out "%{http_code};$LINE;%{redirect_url}\n" "$LINE" >> result.txt
+  curl -o /dev/null --silent --head --write-out "%{http_code};$LINE;%{redirect_url}\n" "$LINE" >> result.csv
 done < urls.txt
