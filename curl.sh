731a943ef148f203f1462a14b6c0516e65a44893
Степан С. Петухов <stepanpetuhov@gmail.com>
2020-09-21 21:59:42 +0300

    Change name of output file

--- curl.sh
+++ curl.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-date > result.txt
+date > result.csv
 while read LINE; do
   echo "$LINE" done!
   curl -o /dev/null --silent --head --write-out "%{http_code};$LINE;%{redirect_url}\n" "$LINE" >> result.txt
